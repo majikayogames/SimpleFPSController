837b9faab44f16a588ee1db2fe9040ea818de349
majikayogames <152851004+majikayogames@users.noreply.github.com>
2024-05-14 07:22:10 -0400

    Fourth tutorial complete

--- FPSController/FPSController.gd
+++ FPSController/FPSController.gd
@@ -16,7 +16,6 @@ var headbob_time := 0.0
 @export var ground_accel := 14.0
 @export var ground_decel := 10.0
 @export var ground_friction := 6.0
-@export var crouch_friction := 3.0
 
 # Air movement settings. Need to tweak these to get the feeling dialed in.
 @export var air_cap := 0.85 # Can surf steeper ramps if this is higher, makes it easier to stick and bhop
@@ -26,6 +25,10 @@ var headbob_time := 0.0
 var wish_dir := Vector3.ZERO
 var cam_aligned_wish_dir := Vector3.ZERO
 
+const CROUCH_TRANSLATE = 0.7
+const CROUCH_JUMP_ADD = CROUCH_TRANSLATE * 0.9 # * 0.9 for sourcelike camera jitter in air on crouch, makes for a nice notifier
+var is_crouched := false
+
 var noclip_speed_mult := 3.0
 var noclip := false
 
@@ -34,6 +37,8 @@ var _snapped_to_stairs_last_frame := false
 var _last_frame_was_on_floor = -INF
 
 func get_move_speed() -> float:
+	if is_crouched:
+		return walk_speed * 0.8
 	return sprint_speed if Input.is_action_pressed("sprint") else walk_speed
 
 func _ready():
@@ -92,7 +97,7 @@ func _save_camera_pos_for_smoothing():
 func _slide_camera_smooth_back_to_origin(delta):
 	if _saved_camera_global_pos == null: return
 	%CameraSmooth.global_position.y = _saved_camera_global_pos.y
-	%CameraSmooth.position.y = clampf(%CameraSmooth.position.y, -0.7, 0.7) # Clamp incase teleported
+	%CameraSmooth.position.y = clampf(%CameraSmooth.position.y, -CROUCH_TRANSLATE, CROUCH_TRANSLATE) # Clamp incase teleported
 	var move_amount = max(self.velocity.length() * delta, walk_speed/2 * delta)
 	%CameraSmooth.position.y = move_toward(%CameraSmooth.position.y, 0.0, move_amount)
 	_saved_camera_global_pos = %CameraSmooth.global_position
@@ -108,8 +113,8 @@ func _snap_down_to_stairs_check() -> void:
 	var floor_below : bool = %StairsBelowRayCast3D.is_colliding() and not is_surface_too_steep(%StairsBelowRayCast3D.get_collision_normal())
 	var was_on_floor_last_frame = Engine.get_physics_frames() == _last_frame_was_on_floor
 	if not is_on_floor() and velocity.y <= 0 and (was_on_floor_last_frame or _snapped_to_stairs_last_frame) and floor_below:
-		var body_test_result = PhysicsTestMotionResult3D.new()
-		if _run_body_test_motion(self.global_transform, Vector3(0,-MAX_STEP_HEIGHT,0), body_test_result):
+		var body_test_result = KinematicCollision3D.new()
+		if self.test_move(self.transform, Vector3(0,-MAX_STEP_HEIGHT,0), body_test_result):
 			_save_camera_pos_for_smoothing()
 			var translate_y = body_test_result.get_travel().y
 			self.position.y += translate_y
@@ -125,15 +130,15 @@ func _snap_up_stairs_check(delta) -> bool:
 	#  We give some clearance above to ensure there's ample room for the player.
 	#  If it hits a step <= MAX_STEP_HEIGHT, we can teleport the player on top of the step
 	#  along with their intended motion forward.
-	var down_check_result = PhysicsTestMotionResult3D.new()
-	if (_run_body_test_motion(step_pos_with_clearance, Vector3(0,-MAX_STEP_HEIGHT*2,0), down_check_result)
+	var down_check_result = KinematicCollision3D.new()
+	if (self.test_move(step_pos_with_clearance, Vector3(0,-MAX_STEP_HEIGHT*2,0), down_check_result)
 	and (down_check_result.get_collider().is_class("StaticBody3D") or down_check_result.get_collider().is_class("CSGShape3D"))):
 		var step_height = ((step_pos_with_clearance.origin + down_check_result.get_travel()) - self.global_position).y
 		# Note I put the step_height <= 0.01 in just because I noticed it prevented some physics glitchiness
 		# 0.02 was found with trial and error. Too much and sometimes get stuck on a stair. Too little and can jitter if running into a ceiling.
 		# The normal character controller (both jolt & default) seems to be able to handled steps up of 0.1 anyway
-		if step_height > MAX_STEP_HEIGHT or step_height <= 0.01 or (down_check_result.get_collision_point() - self.global_position).y > MAX_STEP_HEIGHT: return false
-		%StairsAheadRayCast3D.global_position = down_check_result.get_collision_point() + Vector3(0,MAX_STEP_HEIGHT,0) + expected_move_motion.normalized() * 0.1
+		if step_height > MAX_STEP_HEIGHT or step_height <= 0.01 or (down_check_result.get_position() - self.global_position).y > MAX_STEP_HEIGHT: return false
+		%StairsAheadRayCast3D.global_position = down_check_result.get_position() + Vector3(0,MAX_STEP_HEIGHT,0) + expected_move_motion.normalized() * 0.1
 		%StairsAheadRayCast3D.force_raycast_update()
 		if %StairsAheadRayCast3D.is_colliding() and not is_surface_too_steep(%StairsAheadRayCast3D.get_collision_normal()):
 			_save_camera_pos_for_smoothing()
@@ -142,7 +147,36 @@ func _snap_up_stairs_check(delta) -> bool:
 			_snapped_to_stairs_last_frame = true
 			return true
 	return false
+
+@onready var _original_capsule_height = $CollisionShape3D.shape.height
+func _handle_crouch(delta) -> void:
+	var was_crouched_last_frame = is_crouched
+	if Input.is_action_pressed("crouch"):
+		is_crouched = true
+	elif is_crouched and not self.test_move(self.transform, Vector3(0,CROUCH_TRANSLATE,0)):
+		is_crouched = false
 	
+	# Allow for crouch to heighten/extend a jump
+	var translate_y_if_possible := 0.0
+	if was_crouched_last_frame != is_crouched and not is_on_floor() and not _snapped_to_stairs_last_frame:
+		translate_y_if_possible = CROUCH_JUMP_ADD if is_crouched else -CROUCH_JUMP_ADD
+	# Make sure not to get player stuck in floor/ceiling during crouch jumps
+	if translate_y_if_possible != 0.0:
+		var result = KinematicCollision3D.new()
+		self.test_move(self.transform, Vector3(0,translate_y_if_possible,0), result)
+		self.position.y += result.get_travel().y
+		%Head.position.y -= result.get_travel().y
+		%Head.position.y = clampf(%Head.position.y, -CROUCH_TRANSLATE, 0)
+	
+	%Head.position.y = move_toward(%Head.position.y, -CROUCH_TRANSLATE if is_crouched else 0, 7.0 * delta)
+	$CollisionShape3D.shape.height = _original_capsule_height - CROUCH_TRANSLATE if is_crouched else _original_capsule_height
+	$CollisionShape3D.position.y = $CollisionShape3D.shape.height / 2
+	# Visual for tutorial
+	#$WorldModel/MeshInstance3D.mesh.height = $CollisionShape3D.shape.height
+	#$WorldModel/MeshInstance3D.position.y = $CollisionShape3D.position.y
+	#$WorldModel/WigglyHair.position.y = $CollisionShape3D.shape.height - 0.302
+	#$"WorldModel/disguise-glasses".position.y = $CollisionShape3D.shape.height - 0.9
+
 func _handle_noclip(delta) -> bool:
 	if Input.is_action_just_pressed("_noclip") and OS.has_feature("debug"):
 		noclip = !noclip
@@ -183,13 +217,6 @@ func clip_velocity(normal: Vector3, overbounce : float, delta : float) -> void:
 # Note to followers of my previous tutorials: This function has been simplified but does the same thing.
 func is_surface_too_steep(normal : Vector3) -> bool:
 	return normal.angle_to(Vector3.UP) > self.floor_max_angle
-
-func _run_body_test_motion(from : Transform3D, motion : Vector3, result = null) -> bool:
-	if not result: result = PhysicsTestMotionResult3D.new()
-	var params = PhysicsTestMotionParameters3D.new()
-	params.from = from
-	params.motion = motion
-	return PhysicsServer3D.body_test_motion(self.get_rid(), params, result)
 
 func _handle_air_physics(delta) -> void:
 	self.velocity.y -= ProjectSettings.get_setting("physics/3d/default_gravity") * delta
@@ -245,6 +272,8 @@ func _physics_process(delta):
 	# Depending on which way you have you character facing, you may have to negate the input directions
 	wish_dir = self.global_transform.basis * Vector3(input_dir.x, 0., input_dir.y)
 	cam_aligned_wish_dir = %Camera3D.global_transform.basis * Vector3(input_dir.x, 0., input_dir.y)
+	
+	_handle_crouch(delta)
 	
 	if not _handle_noclip(delta):
 		if is_on_floor() or _snapped_to_stairs_last_frame:
